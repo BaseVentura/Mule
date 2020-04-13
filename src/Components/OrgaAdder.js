8f66fecc71624ded381c435cb94fd0bb2f9f4ade
BaseVentura <max.stricker@gmx.net>
2020-04-13 20:28:18 +0200

    Fixed Input Value Problem

--- src/Components/OrgaAdder.js
+++ src/Components/OrgaAdder.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './OrgaAdder.module.css'
 
 function OrgaAdder(props) {
+
+
+    const [name, setName] =useState('');
+    const [url, setUrl] =useState('');
+    const [label, setLabel] =useState('');
+
+    
     return (
         <div className={styles.OrgaAdder}>
         <div className="title">Organisation hinzufügen</div>
          <div>
-            <input id="Name" type="text" defaultValue="Enter Name"></input>
+            <input onChange={(event) => setName(event.target.value)} type="text" defaultValue="Enter Name"></input>
          </div>
          <div>   
-            <input type="text" defaultValue="Enter URL"></input>
+            <input onChange={(event)=> setUrl(event.target.value)} type="text" defaultValue="Enter URL"></input>
         </div>
         <div> 
-         <input type="text" defaultValue="Enter Label"></input>
+         <input onChange={(event)=> setLabel(event.target.value)} type="text" defaultValue="Enter Label"></input>
         </div>
-         <button onClick={()=>props.click(("newOrga", "New URL", "NewLabel"))}>Add</button> 
+         <button onClick={()=>props.click(name, url, label)}>Add</button> 
         </div>
     );
 }
