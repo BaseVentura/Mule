a8ecf64bb555ae68267ec2ec5647d72b7ddd258a
BaseVentura <max.stricker@gmx.net>
2020-05-05 22:28:00 +0200

    renamed label and orga and label const in activeLabel

--- src/Components/OrgaList.js
+++ src/Components/OrgaList.js
@@ -3,21 +3,20 @@ import Organisation from './Organisation'
 // import styles from '../App.module.css'
 
 
-function OrgaList({Labels,Orgas,filter,clickDelete}) {
+function OrgaList({labels,orgas,filter,clickDelete}) {
   //  let label=null;
     return (
         <div >
-            {Orgas.map((orga) => {
+            {orgas.map((orga) => {
               
-              var labels = Labels.filter((label)=> orga.LabelIds.includes(label.id));
-              
-              const inFilter = labels.some(label=> filter.includes(label.id))  
+              const activeLabels = labels.filter((label)=> orga.LabelIds.includes(label.id));
+              const inFilter = activeLabels.some(label=> filter.includes(label.id))  
               
               return (
               inFilter || filter.length===0 ? <Organisation
                 name = {orga.name}
                 url ={orga.URL}
-                labels = {labels}
+                labels = {activeLabels}
                 key={orga.Id}
                 id={orga.id}
                 clickDelete={clickDelete}
