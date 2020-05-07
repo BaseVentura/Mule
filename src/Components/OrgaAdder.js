1d6e1d38f1dd1bbef5ffaf43cf8c50ba245d6203
BaseVentura <max.stricker@gmx.net>
2020-05-07 15:32:04 +0200

    Open Orgaadder per defaut in Orgalist

--- src/Components/OrgaAdder.js
+++ src/Components/OrgaAdder.js
@@ -3,9 +3,7 @@ import styles from './OrgaAdder.module.css'
 import LabelControl from './LabelControl';
 import PropTypes from 'prop-types';
 
-function OrgaAdder({labels,hide,click,show}) {
-
-    const showHideClassName = show ? styles.showDiv : styles.hideDiv;
+function OrgaAdder({labels,click}) {
     
     const [name,setName] = useState();
     const [URL,setURL] = useState();
@@ -27,11 +25,8 @@ function OrgaAdder({labels,hide,click,show}) {
     }
 
     return (
-        <div className={showHideClassName}>
-            <div className={styles.modal}>
-                <div className={styles.modalContent}>    
-                    <div className={styles.modalHeader}>
-                        <span className={styles.close} onClick={hide}>×</span>
+            <div className={styles.OrgaAdder}>   
+                    <div className={styles.Title}>
                         Organisation hinzufügen     
                     </div>
                     <div>
@@ -49,22 +44,18 @@ function OrgaAdder({labels,hide,click,show}) {
                             id={id}  
                             clicked={(event)=> buildLabel(id)}/>
                             )
-                        
                         )
                     }
                     </div>
                     <button onClick={()=>click(name, URL, Label, Date.now())}>Add</button> 
-                </div>
+                
             </div>
-        </div>
     );
 }
 
 OrgaAdder.propTypes = {
     labels: PropTypes.array.isRequired,
-    hide: PropTypes.func.isRequired,
     click: PropTypes.func.isRequired,
-    show: PropTypes.bool.isRequired
 }
 
 export default OrgaAdder;
