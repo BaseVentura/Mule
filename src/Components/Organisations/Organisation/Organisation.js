30a6d7c460e24b2db98dfdb5413fe8e8172a0053
BaseVentura <max.stricker@gmx.net>
2020-05-14 23:54:10 +0200

    Added Aux Component and started work on modal

--- src/Components/Organisations/Organisation/Organisation.js
+++ src/Components/Organisations/Organisation/Organisation.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import style from './Organisation.module.css'
 import PropTypes from 'prop-types';
+import Aux from '../../../hoc/Aux'
+import Modal from '../../UI/Modal/Modal'
 
 function Organisation({name,clickDelete,id,url,labels}) {
+    
     return (
+        <Aux>
+            {/* <Modal show> Modal in Organisation</Modal> */}
             <div className ={style.Orga}>
-            <div className={style.Title}>{name}<span className={style.close} onClick={()=> clickDelete(id)}>×</span></div>
-            <p>Website: <a href={url}>  {url}</a></p>
-            <span>
-                {labels.map((label)=>{return (<div className={style.Badge} key={label.id}>{label.name}</div>);})}
-            </span>
-        </div>
+                <div className={style.Title}>{name}<span className={style.close} onClick={()=> clickDelete(id)}>×</span></div>
+                <p>Website: <a href={url}>  {url}</a></p>
+                <span>
+                    {labels.map((label)=>{return (<div className={style.Badge} key={label.id}>{label.name}</div>);})}
+                </span>
+            </div>
+        </Aux>
     );
 }
 Organisation.propTypes={
