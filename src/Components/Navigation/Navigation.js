69ad7b98bcf35979b9497d01bd5588b988812663
BaseVentura <max.stricker@gmx.net>
2020-05-15 17:01:25 +0200

    Added nav.js

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux'
+
+const Navigation = (props) => {
+    return (
+        <Aux>
+        <div>
+            Logo
+        </div>
+        <div>
+            Name
+        </div>
+        <div>
+            NavLinks
+        </div>
+        </Aux>
+    );
+}
+
+export default Navigation;
