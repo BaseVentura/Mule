2e79069ea8a5349514dee428087713f568359390
BaseVentura <max.stricker@gmx.net>
2020-05-29 02:45:48 +0200

    started to render labels

--- src/Components/VolunteerJobs/VolunteerJobList/VolunteerJobList.js
+++ src/Components/VolunteerJobs/VolunteerJobList/VolunteerJobList.js
@@ -3,11 +3,15 @@ import React from 'react';
 import style from './VolunteerJobList.module.css'
 
 const VolunteerJobList = ({jobs,labels}) => {
+     
+    
     return (
     jobs.map((job)=> <div className={style.Job}>
-            <div className={style.Title}>{job.name}<span className={style.close}>X</span></div>
+            <div key={job.id} className={style.Title}>{job.name}<span className={style.close}>X</span></div>
             <div>{job.description}</div>
-            {/* {labels.find()}  */}
+    {jobs.map(job => labels.filter(label => job.LabelIds.includes(label.id))).map((activeLabel)=> {
+        console.log(activeLabel)
+        return <div>{activeLabel[0].name}</div>})}
         </div>)
     );
 };
