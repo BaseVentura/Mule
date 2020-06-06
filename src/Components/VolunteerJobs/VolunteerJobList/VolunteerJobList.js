c4f582ec10d645381428f8dff0d8d71db34ed63b
BaseVentura <max.stricker@gmx.net>
2020-06-06 22:58:32 +0200

    finished Joblist

--- src/Components/VolunteerJobs/VolunteerJobList/VolunteerJobList.js
+++ src/Components/VolunteerJobs/VolunteerJobList/VolunteerJobList.js
@@ -1,19 +1,44 @@
 import React from 'react';
+import VolunteerJob from '../VolunteerJob/VolunteerJob'
+import PropTypes from 'prop-types';
+import styles from './VolunteerJobList.module.css'
+import OrgaAdder from '../../OrgaAdder/OrgaAdder'
 
-import style from './VolunteerJobList.module.css'
 
-const VolunteerJobList = ({jobs,labels}) => {
-     
-    
+function VolunteerJobList({labels,orgas,filter,clickDelete,addOrga,jobs}) {
+  //  let label=null;
     return (
-    jobs.map((job)=> <div className={style.Job}>
-            <div key={job.id} className={style.Title}>{job.name}<span className={style.close}>X</span></div>
-            <div>{job.description}</div>
-    {jobs.map(job => labels.filter(label => job.LabelIds.includes(label.id))).map((activeLabel)=> {
-        console.log(activeLabel)
-        return <div>{activeLabel[0].name}</div>})}
-        </div>)
-    );
-};
+        <div className={styles.content}>
+            {jobs.map((job) => {
+              
+              const activeLabels = labels.filter((label)=> job.LabelIds.includes(label.id));
+              const inFilter = activeLabels.some(label=> filter.includes(label.id));
+              
+              const activeOrga = orgas.find(orga => orga.Id===job.OrgaId);
+              
+
+              
+              return (
+              inFilter || filter.length===0 ? <VolunteerJob
+                name = {job.name}
+                description ={job.description}
+                labels = {activeLabels}
+                key={job.id}
+                orga={activeOrga}
+                id={job.id}
+                clickDelete={clickDelete}
+                /> : null
+              );  
+            })}
+            <OrgaAdder labels={labels} click={addOrga}/>
+        </div>
+      );
+}
+VolunteerJobList.propTypes={
+  orgas: PropTypes.arrayOf(PropTypes.object).isRequired,
+  filter: PropTypes.arrayOf(PropTypes.number).isRequired,
+  clickDelete: PropTypes.func,
+  Labels: PropTypes.arrayOf(PropTypes.object)
+}
 
 export default VolunteerJobList;
