c493b29bebcd57f5bca29b7a66a18d851d3e8526
BaseVentura <max.stricker@gmx.net>
2020-07-20 21:54:59 +0200

    created API-fn (fauna) "do getAll and delete

---
+++ src/API/index.js
@@ -0,0 +1,26 @@
+import {client, q} from '../config/db';
+
+export const getAllOrgas = client.query(q.Paginate(q.Match(q.Ref('indexes/all_orgas'))))
+    .then(response => {
+        const orgaRef = response.data;
+        const getAllDataQuery = orgaRef.map(ref => {
+            return q.Get(ref);
+        });
+        return client.query(getAllDataQuery).then(data => data);
+    })
+    .catch(error => console.error('Error: ', error.message));
+
+export const createOrga = orga => {
+    client.query(
+        q.Create(q.Collection('Organisations'), {
+            data: { orga}
+        },)
+    )
+    .then(ret => console.log('Response: ', ret))
+    .catch(error => console.error('Error in createOrga: ', error.message));
+};
+export const deleteOrga = orgaId => {
+    client.query(q.Delete(q.Ref(q.Collection('Organisation'), orgaId)))
+    .then(ret => ret)
+    .catch(error => console.error('Error in deleteOrga: ', error.message));
+};
