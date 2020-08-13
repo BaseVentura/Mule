d43bdd9dd306be174298b7ce3b61a53b92a87e16
BaseVentura <max.stricker@gmx.net>
2020-08-13 22:53:13 +0200

    coe formatting and changed return statement

--- src/API/index.js
+++ src/API/index.js
@@ -1,26 +1,34 @@
-import {client, q} from '../config/db';
+import { client, q } from "../config/db";
 
-export const getAllOrgas = client.query(q.Paginate(q.Match(q.Ref('indexes/all_orgas'))))
-    .then(response => {
-        const orgaRef = response.data;
-        const getAllDataQuery = orgaRef.map(ref => {
-            return q.Get(ref);
-        });
-        return client.query(getAllDataQuery).then(data => data);
-    })
-    .catch(error => console.error('Error: ', error.message));
+export const getAllOrgas = client
+  .query(q.Paginate(q.Match(q.Ref("indexes/all_orgas"))))
+  .then((response) => {
+    const orgaRef = response.data;
+    const getAllDataQuery = orgaRef.map((ref) => {
+      return q.Get(ref);
+    });
+    return client.query(getAllDataQuery).then((data) => data);
+  })
+  .catch((error) => console.error("Error: ", error.message));
 
-export const createOrga = orga => {
-    client.query(
-        q.Create(q.Collection('Organisations'), {
-            data: orga
-        },)
-    )
-    .then(ret => console.log('Response: ', ret))
-    .catch(error => console.error('Error in createOrga: ', error.message));
+export const createOrga = (orga) => {
+  return (
+    client
+      .query(
+        q.Create(q.Collection("Organisations"), {
+          data: orga,
+        })
+      )
+      /*.then((ret) => {
+      //console.log("Response: ", ret);
+      //return ret;
+    })*/
+      .catch((error) => console.error("Error in createOrga: ", error.message))
+  );
 };
-export const removeOrga = orgaId => {
-    client.query(q.Delete(q.Ref(q.Collection('Organisations'), orgaId)))
-    .then(ret => ret)
-    .catch(error => console.error('Error in deleteOrga: ', error.message));
+export const removeOrga = (orgaId) => {
+  client
+    .query(q.Delete(q.Ref(q.Collection("Organisations"), orgaId)))
+    .then((ret) => ret)
+    .catch((error) => console.error("Error in deleteOrga: ", error.message));
 };
