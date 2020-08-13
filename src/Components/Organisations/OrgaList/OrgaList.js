abe4aab8a30594df635d6f6c42aa6ee744284716
BaseVentura <max.stricker@gmx.net>
2020-08-13 22:53:46 +0200

    code formatting

--- src/Components/Organisations/OrgaList/OrgaList.js
+++ src/Components/Organisations/OrgaList/OrgaList.js
@@ -1,39 +1,42 @@
-import React from 'react';
-import Organisation from '../Organisation/Organisation'
-import PropTypes from 'prop-types';
-import styles from './Orgalist.module.css'
-import OrgaAdder from '../../OrgaAdder/OrgaAdder'
+import React from "react";
+import Organisation from "../Organisation/Organisation";
+import PropTypes from "prop-types";
+import styles from "./Orgalist.module.css";
+import OrgaAdder from "../../OrgaAdder/OrgaAdder";
 
-
-function OrgaList({labels,orgas,filter,clickDelete,addOrga}) {
+function OrgaList({ labels, orgas, filter, clickDelete, addOrga }) {
   //  let label=null;
-    return (
-        <div className={styles.content}>
-            {orgas.map((orga) => {
-              
-              const activeLabels = labels.filter((label)=> orga.LabelIds.includes(label.id));
-              const inFilter = activeLabels.some(label=> filter.includes(label.id))  
-              
-              return (
-              inFilter || filter.length===0 ? <Organisation
-                name = {orga.name}
-                url ={orga.URL}
-                labels = {activeLabels}
-                key={orga.Id}
-                id={orga.Id}
-                clickDelete={clickDelete}
-                /> : null
-              );  
-            })}
-            <OrgaAdder labels={labels} click={addOrga}/>
-        </div>
-      );
+  console.log(orgas);
+  return (
+    <div className={styles.content}>
+      {orgas.map((orga) => {
+        const activeLabels = labels.filter((label) =>
+          orga.LabelIds.includes(label.id)
+        );
+        const inFilter = activeLabels.some((label) =>
+          filter.includes(label.id)
+        );
+
+        return inFilter || filter.length === 0 ? (
+          <Organisation
+            name={orga.name}
+            url={orga.URL}
+            labels={activeLabels}
+            key={orga.Id}
+            id={orga.Id}
+            clickDelete={clickDelete}
+          />
+        ) : null;
+      })}
+      <OrgaAdder labels={labels} click={addOrga} />
+    </div>
+  );
 }
-OrgaList.propTypes={
+OrgaList.propTypes = {
   orgas: PropTypes.arrayOf(PropTypes.object).isRequired,
   filter: PropTypes.arrayOf(PropTypes.number).isRequired,
   clickDelete: PropTypes.func,
-  Labels: PropTypes.arrayOf(PropTypes.object)
-}
+  Labels: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default OrgaList;
